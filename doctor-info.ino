10db9414fb90f8a47ccdb940691eb760b3c4a522
MBronars <35738375+MBronars@users.noreply.github.com>
2018-03-06 21:55:06 -0800

    Create doctor-info.ino

---
+++ doctor-info.ino
@@ -0,0 +1,19 @@
+int ledPin = 13;
+
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+ 
+}
+
+void loop() {
+  // read the value from the sensor:
+  
+  // turn the ledPin on
+  
+  digitalWrite(ledPin, HIGH);
+  
+
+  
+}

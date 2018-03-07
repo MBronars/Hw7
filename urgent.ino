a6ca2d87537b49e7b3762669941af88320dd0002
MBronars <35738375+MBronars@users.noreply.github.com>
2018-03-06 21:51:45 -0800

    Create urgent.ino

---
+++ urgent.ino
@@ -0,0 +1,21 @@
+int ledPin = 13;
+
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+ 
+}
+
+void loop() {
+  // read the value from the sensor:
+  
+  // turn the ledPin on
+  
+  digitalWrite(ledPin, HIGH);
+  delay(50);
+  digitalWrite(ledPin, LOW);
+  delay(50);
+
+  
+}

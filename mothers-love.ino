b4831a19f1db980ff2ec6128f83f1ee8771e0803
MBronars <35738375+MBronars@users.noreply.github.com>
2018-03-06 21:54:30 -0800

    Create mothers-love.ino

---
+++ mothers-love.ino
@@ -0,0 +1,25 @@
+int ledPin = 13;
+
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+ 
+}
+
+void loop() {
+  // read the value from the sensor:
+  
+  // turn the ledPin on
+  
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+  digitalWrite(ledPin, HIGH);
+  delay(2000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+
+  
+}
